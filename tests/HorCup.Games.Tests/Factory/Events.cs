e0af7122df210bf10bc08073643ec53c2e9f07be
Artem Horbunov <gorbunowartemwork@gmail.com>
2021-11-15 10:35:12 +0200

    Removed SearchProjection tests

--- tests/HorCup.Games.Tests/Factory/Events.cs
+++ tests/HorCup.Games.Tests/Factory/Events.cs
@@ -1,34 +1,35 @@
-﻿using HorCup.Games.Events;
+﻿using AutoFixture;
+using HorCup.Games.Events;
 
 namespace HorCup.Games.Tests.Factory
 {
 	internal class Events
 	{
 		private readonly GamesFactory _factory;
+		private readonly Fixture _fixture;
 
 		public Events(GamesFactory factory)
 		{
 			_factory = factory;
+			_fixture = new Fixture();
 		}
 
-		public GameCreated GameCreated() => new()
-		{
-			Id = _factory.Id,
-			Genre = GamesFactory.Genre,
-			MaxPlayers = GamesFactory.MaxPlayers,
-			MinPlayers = GamesFactory.MinPlayers,
-		};
+		public GameCreated GameCreated() => _fixture.Build<GameCreated>()
+			.With(g => g.Id, _factory.Id)
+			.With(g => g.Genre, GamesFactory.Genre)
+			.With(g => g.MaxPlayers, GamesFactory.MaxPlayers)
+			.With(g => g.MinPlayers, GamesFactory.MinPlayers)
+			.Create();
 
-		public GameTitleSet GameTitleSet() => new()
-		{
-			Id = _factory.Id,
-			Title = GamesFactory.Title
-		};
+		public GameTitleSet GameTitleSet() => _fixture.Build<GameTitleSet>()
+			.With(g => g.Id, _factory.Id)
+			.With(g => g.Title, GamesFactory.Title)
+			.Create();
 
-		public GameDescriptionChanged GameDescriptionChanged() => new()
-		{
-			Id = _factory.Id,
-			Description = GamesFactory.Description
-		};
+		public GameDescriptionChanged GameDescriptionChanged() =>
+			_fixture.Build<GameDescriptionChanged>()
+				.With(g => g.Id, _factory.Id)
+				.With(g => g.Description, GamesFactory.Description)
+				.Create();
 	}
 }
